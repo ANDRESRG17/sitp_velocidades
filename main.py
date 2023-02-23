01b9db4a1913724ba25968281c847f9fb125e957
Cristian Ríos Hernández <69606301+CristianRiosH@users.noreply.github.com>
2023-02-23 06:07:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from src import get_data
+from src.get_data import positions, shape, speeds, union, insert_function ######> ajustar nombrees de funciones de acuerdo a los cambios en get_data()
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+######> en este archivo se deberian traer las fucniones de get_data y orquestar el fuljo de trabajo, algo así:
 
 
-from src.get_data import conexion_bq
+def sitp_speeds_workflow():
+  """docuementar...
+  """
+   raw_data       = positions()
+   geo_info       = shape()
+   data_processed = speeds(raw_data)
+   data_speeds    = union(data_processed, geo_info)
+   insert_function(data_speeds)
+
+if __name__ == "__main__":
+  sitp_speeds_workflow()
+  
